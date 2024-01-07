b1173b2aa9610c2e769637c9591c5617bdb88a34
Sebastian Sloniec <sebastian.sloniec@gmail.com>
2024-01-07 14:22:48 +0100

    feat: add Money value object class basic implementation

---
+++ src/money/money.ts
@@ -0,0 +1,41 @@
+export class Money {
+  private value: number;
+
+  constructor(value: number) {
+    this.value = value;
+  }
+
+  public add(money: Money): Money {
+    return new Money(this.value + money.value);
+  }
+
+  public subtract(money: Money): Money {
+    return new Money(this.value - money.value);
+  }
+
+  public percentage(percent: number): Money {
+    return new Money(Math.round(this.value * (percent / 100)));
+  }
+
+  public equals(money: Money): boolean {
+    if (this === money) {
+      return true;
+    }
+
+    if (!money || money.constructor !== this.constructor) {
+      return false;
+    }
+
+    return this.value === money.value;
+  }
+
+  public toString() {
+    const value = this.value.toFixed(2);
+
+    return `${value}`;
+  }
+
+  public toInt(): number {
+    return this.value;
+  }
+}
